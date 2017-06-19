9b8baefba802efd70c5c4c293f4af1f15d410cf8
Joel Bakken and Mick Sexton <mick@>
2017-06-19 13:11:56 -0700

    Add some UI in the javascript,variable names and inputs for new objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 // BUSINESS LOGIC
 
 //Pet constructor
-function Pet(petName, petAge, petSpecies, petStatus) {
+function Pet(petName, petAge, petSpecies) {
   this.petName = petName;
   this.petAge = petAge;
   this.petSpecies = petSpecies;
@@ -9,21 +9,23 @@ function Pet(petName, petAge, petSpecies, petStatus) {
 }
 
 //prototype to change status
-Pet.prototype.changeStatus() {
+Pet.prototype.changeStatus = function() {
   this.petStatus = "adopted";
 }
 
 //prototype to get status
-Pet.prototype.getStatus() {
+Pet.prototype.getStatus = function() {
   return this.petStatus;
 }
 
 //sort pets into available or adopted arrays accordingly, pass in pet object
-function sortPets(pet) {
-  if (pet.petStatus === "available") {
-    availablePets.push(pet);
-  } else if (pet.petStatus === "adopted") {
-    adoptedPets.push(pet);
+function sortPets() {
+  for(var i=0; i < allPets.length; i++) {
+    if (allPets[i].getStatus === "available") {
+      availablePets.push(allPets[i]);
+    } else if (allPets[i].petStatus === "adopted") {
+      adoptedPets.push(allPets[i]);
+    }
   }
 }
 
@@ -33,3 +35,25 @@ var availablePets = [];
 var adoptedPets = [];
 
 //USER INTERFACE LOGIC
+$(document).ready(function() {
+    $("form#new-pet-form").submit(function(event) {
+      event.preventDefault();
+      //reset Available and Adopted Pet Arrays, not All Pets
+      availablePets = [];
+      adoptedPets = [];
+      //input from fields
+      var name = $("#new-pet-name").val();
+      var age = $("#new-pet-age").val();
+      var species = $("input:radio[name=species]:checked").val();
+
+      //create Pet object
+      var newPet = new Pet(name, age, species);
+      //populate All Pets array with new Pet object
+      allPets.push(newPet);
+      //sort newly initialized Available and Adopted Pet arrays accordingly
+      sortPets();
+
+      //console.log(allPets[0].petStatus);
+
+  });
+});
